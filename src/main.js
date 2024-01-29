20de4acf0dddf7de838e8a3b09fb933662e38697
irinakurnishova <kurnishovaiv@gmail.com>
2024-01-29 23:18:29 +0100

    main app settings

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './main.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+import "bootstrap-icons/font/bootstrap-icons.css"
 import store from './store/store.js'
 import App from './App.vue'
 
