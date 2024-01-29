07dd70f4e718266202346e14366b02defaf92a48
irinakurnishova <kurnishovaiv@gmail.com>
2024-01-29 23:21:22 +0100

    tasks list component

--- src/components/tasks-list.vue
+++ src/components/tasks-list.vue
@@ -2,21 +2,29 @@
   <section class="todo-list">
     <h4>TODO LIST</h4>
     <div class="list">
-      <task v-for="task in tasks" :id="task.id" @remove-task="removeTask"/>
+      <task v-for="task in tasks" :task="task" @remove-task="removeTask" @update-task="saveUpdates"/>
     </div>
   </section>
 </template>
 
 <script setup>
-import { useStore } from 'vuex'
-import {computed} from "vue"
+import { useStore } from "vuex"
 import Task from "./task.vue"
 
 const store = useStore()
-const tasks = computed(() => store.getters.getTasks.sort((a, b) => {
-  return b.createdAt - a.createdAt
-}))
-const removeTask = (id) => {
-  store.dispatch('deleteTask', id)
+const emit = defineEmits(['taskUpdated', 'taskDeleted'])
+
+defineProps({
+  tasks: Array
+})
+
+const removeTask = async (id) => {
+  await store.dispatch('deleteTask', id)
+  emit('taskDeleted')
+}
+
+const saveUpdates = async (updatedTask) => {
+  await store.dispatch('updateTask', updatedTask)
+  emit('taskUpdated')
 }
 </script>
