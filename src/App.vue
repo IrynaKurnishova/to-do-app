aa856ac7a99c5f2dd4ad8053b4d80f5bf3f82f89
irinakurnishova <kurnishovaiv@gmail.com>
2024-01-29 23:20:44 +0100

    main page created

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,63 @@
 <template>
   <main class="card">
+    <div>
+
+    </div>
     <section class="greeting">
+      <alert :success-alert="successAlert" :alert-text="updates"/>
       <h2 class="title">
         What's up,
         <input
           type="text"
-          placeholder="Name here"
+          placeholder="input name here"
           v-model="name"
         />
       </h2>
     </section>
 
-    <create-task />
+    <create-task @task-created="fetchData"/>
 
-    <tasks-list/>
+    <tasks-list
+        :tasks="tasks"
+        @task-updated="showAlert('updated')"
+        @task-deleted="showAlert('deleted')"
+    />
   </main>
 </template>
 
 <script setup>
 import {ref, watch, onMounted} from 'vue'
+import {useStore} from "vuex"
 import CreateTask from "./components/create-task.vue"
 import TasksList from "./components/tasks-list.vue"
-const name = ref('')
+import Alert from "./components/alert.vue"
 
-// const updateLocalStorage = () => {
-//   localStorage.setItem('todos', JSON.stringify(todos.value))
-// }
-//
+const name = ref('')
+const store = useStore()
+const tasks = ref([])
+const successAlert = ref(false)
+const updates = ref('')
+
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
 })
-//
-// watch(todos, newVal => {
-//   localStorage.setItem('todos', JSON.stringify(newVal))
-// }, {deep: true})
-//
+const fetchData = async () => {
+  tasks.value = await store.dispatch('fetchData')
+};
+
+onMounted(fetchData)
+
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
-  // todos.value = JSON.parse(localStorage.getItem('todos')) || []
 })
+
+const showAlert = (value) => {
+  successAlert.value = true
+  updates.value = value
+  fetchData()
+  setTimeout(() => {
+    successAlert.value = false
+  }, 2000)
+}
 </script>
 
